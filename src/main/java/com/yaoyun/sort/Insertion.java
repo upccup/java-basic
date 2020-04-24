491a213614556342dd5ebed5118fc37bf5b4eea6
姚云 <yy152986@alibaba-inc.com>
2020-04-24 15:13:04 +0800

    add insertSort2

--- src/main/java/com/yaoyun/sort/Insertion.java
+++ src/main/java/com/yaoyun/sort/Insertion.java
@@ -16,7 +16,21 @@ public class Insertion {
                 }
             }
         }
-        
+
+        return nums;
+    }
+
+    public static int[] insertion2(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int temp = nums[i];
+            int j = i - 1;
+            while (j >= 0 && nums[j] > temp) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+
+            nums[j + 1] = temp;
+        }
         return nums;
     }
 }
--- src/test/java/com/yaoyun/sort/InsertionTest.java
+++ src/test/java/com/yaoyun/sort/InsertionTest.java
@@ -24,4 +24,19 @@ public class InsertionTest {
         assertEquals(-8, result[1],0);
         assertEquals(2, result[3],0);
     }
+
+    @Test
+    public void insertion2() {
+        int[] result = Insertion.insertion2(new int[] {5, 2, 3, 1});
+        assertEquals(1, result[0],0);
+        assertEquals(2, result[1],0);
+        assertEquals(5, result[3],0);
+        result = Insertion.insertion2(new int[] {5,1,1,2,0,0});
+        assertEquals(0, result[0],0);
+        assertEquals(0, result[1],0);
+        assertEquals(5, result[result.length-1],0);
+        result = Insertion.insertion2(new int[] {-1, 2, -8, -10});
+        assertEquals(-8, result[1],0);
+        assertEquals(2, result[3],0);
+    }
 }
